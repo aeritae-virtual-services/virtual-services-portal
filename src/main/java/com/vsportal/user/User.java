bf31a3945fc9cf2f739e22d5ccf5e0f02377b1c6
zorgdrager <jzorgdrager@aeritae.com>
2016-02-16 12:10:36 -0600

    base user object

--- src/main/java/com/vsportal/user/User.java
+++ src/main/java/com/vsportal/user/User.java
@@ -1,5 +1,81 @@
 package com.vsportal.user;
 
 public class User {
+	
+	private int id;
+	private String first_name;
+	private String last_name;
+	private String phone;
+	private String email;
+	private int companyID;
+	private int roleID;
+	
+	public User(){
+	}
+	
+	public User(String first_name, String last_name, String phone, String email, int companyID, int roleID){
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.phone = phone;
+		this.email = email;
+		this.companyID = companyID;
+		this.roleID = roleID;
+	}
+	
+	
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getCompanyID() {
+		return companyID;
+	}
+
+	public void setCompanyID(int companyID) {
+		this.companyID = companyID;
+	}
+
+	public int getRoleID() {
+		return roleID;
+	}
+
+	public void setRoleID(int roleID) {
+		this.roleID = roleID;
+	}
+	
+	
 
 }
--- src/main/java/com/vsportal/user/UserDAO.java
+++ src/main/java/com/vsportal/user/UserDAO.java
@@ -1,5 +1,6 @@
 package com.vsportal.user;
 
+
 public class UserDAO {
 	
 }
