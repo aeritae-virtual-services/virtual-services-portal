5258476ca7e327a635df3bd84c010ee4b285374a
nzitzer <nczitzer@Nicholass-MacBook-Pro.local>
2016-02-16 13:37:31 -0600

    Session work

--- src/main/java/com/vsportal/session/LoginController.java
+++ src/main/java/com/vsportal/session/LoginController.java
@@ -3,7 +3,10 @@ package com.vsportal.session;
 import org.springframework.stereotype.Controller;  
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView; 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+import java.util.Map;
 
 import com.vsportal.session.*;
 
@@ -11,4 +14,24 @@ import com.vsportal.session.*;
 @RequestMapping("/login")
 public class LoginController {
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public String showForm(Map model) {
+		Login login = new Login();
+		model.put("login", login);
+		return "login";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String processForm(@Valid Login login, BindingResult result, Map Model) {
+		String username = "username";
+		String password = "password";
+		
+		if(result.hasErrors()) {
+			return "login";
+		}
+		
+		login = (Login) model.get("login");
+		
+		if(!login.getUsername().equals(username) || )
+	}
 }
--- src/main/java/com/vsportal/user/UserDAO.java
+++ src/main/java/com/vsportal/user/UserDAO.java
@@ -1,6 +1,44 @@
 package com.vsportal.user;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.vsportal.jdbc.ConnectionPool;
+import com.vsportal.jdbc.DBUtil;
 
 public class UserDAO {
-	
+    
+	public User getUserByUsername(String username) {
+		ConnectionPool pool = ConnectionPool.getInstance();
+	    Connection connection = pool.getConnection();
+	    PreparedStatement ps = null;
+	    ResultSet rs = null;
+	    User user;
+		String query = "SELECT * from User WHERE username = ?";
+		
+		try {        
+            ps = connection.prepareStatement(query);
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+            
+            if(rs.next()) {
+            	user = new User();
+            } 
+            
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } finally {
+        	DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+		
+		return user;
+	}
+    
+    
+    
+    
 }
