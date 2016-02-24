a55ab3cc8e3692fdb16e65ce8af4474a3070b002
TylerSeanRau <trau@aeritae.com>
2016-02-25 00:43:44 +0900

    Updated ApprovalController.java

--- src/main/java/com/vsportal/approval/ApprovalController.java
+++ src/main/java/com/vsportal/approval/ApprovalController.java
@@ -1,8 +1,190 @@
 package com.vsportal.approval;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vsportal.approval.Approval;
+import com.vsportal.approval.ApprovalDAO;
+import com.vsportal.user.UserDAO;
+import com.vsportal.utils.SessionHelper;
 
 @Controller
 public class ApprovalController {
-	//TODO Tyler
+
+	//Display List For: approval
+    @RequestMapping(value = "/approval_list", params = {"query"}, method = RequestMethod.GET)
+    public ModelAndView displayList(HttpServletRequest request, @RequestParam(value = "query") String query) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: approval
+    	ApprovalDAO approvalDAO = new ApprovalDAO();
+    	
+    	//Validate Session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to list for: Approval
+	    	model.addObject("redirectTo", "/approval_list?query=" + query);
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Get ArrayList From Query For: Approval
+	    	ArrayList<Approval> approvalList = ApprovalDAO.getListByQuery(query);
+		    //Update session user's personalized filter from the provided query for: Approval
+		    userSessionDAO.updateUsersFilter("Approval", query);
+	    	
+	    	//Call List View For: Approval
+	    	model = new ModelAndView("Approval_list");
+	    	//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass ArrayList to View For: Approval
+	    	model.addObject("approvalList", approvalList);
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Add Form For: Approval
+    @RequestMapping(value = "/add_approval", method = RequestMethod.GET)
+    public ModelAndView displayNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: Approval
+    		model.addObject("redirectTo", "/add_approval");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: Approval
+    		model = new ModelAndView("approval_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+    		//Pass Operation of New
+    		model.addObject("operation", "new");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Add Form For: approval
+    @RequestMapping(value = "/add_approval", method = RequestMethod.POST)
+    public ModelAndView insertNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: Approval
+    	ApprovalDAO approvalDAO = new ApprovalDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: approval
+    		model.addObject("redirectTo", "/add_approval");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not created.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: approval
+    		
+    		//TODO Insert Base Object For: approval
+    		
+    		//Redirect to list for: approval
+    		//Include standard filter for: approval
+    		model = new ModelAndView("redirect:/approval_list?query=" + userSessionDAO.getUsersFilter("Approval"));
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Update Form For: Approval
+    @RequestMapping(value = "/update_approval", params = {"id"}, method = RequestMethod.GET)
+    public ModelAndView displayExistingUser(HttpServletRequest request, @RequestParam(value = "id") Integer id) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: approval
+    		model.addObject("redirectTo", "/update_approval?id=" +  id.toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: approval
+    		model = new ModelAndView("approval_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass Operation of Update
+    		model.addObject("operation", "update");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Update Form For: approval
+    @RequestMapping(value = "/update_approval", method = RequestMethod.POST)
+    public ModelAndView updateExistingUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: approval
+    	ApprovalDAO approvalDAO = new ApprovalDAO();
+    	
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: approval
+    		model.addObject("redirectTo", "/update_approval?id=" +  request.getParameter("id").toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not updated.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: Approval
+    		
+    		//TODO Update Base Object For: Approval
+    		
+    		//Redirect to list for: Approval
+    		//Include standard filter for: Approval
+    		model = new ModelAndView("redirect:/approval_list?query=" + userSessionDAO.getUsersFilter("Approval"));
+    	}
+    	
+    	return model;
+    }
 }
