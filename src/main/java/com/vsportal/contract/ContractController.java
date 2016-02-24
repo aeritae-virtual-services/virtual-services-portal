232f21c4043469b4734810d315261b472a2e396f
TylerSeanRau <trau@aeritae.com>
2016-02-25 00:56:42 +0900

    Updated ContractController.java

--- src/main/java/com/vsportal/contract/ContractController.java
+++ src/main/java/com/vsportal/contract/ContractController.java
@@ -1,9 +1,190 @@
 package com.vsportal.contract;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vsportal.contract.Contract;
+import com.vsportal.contract.ContractDAO;
+import com.vsportal.user.UserDAO;
+import com.vsportal.utils.SessionHelper;
 
 @Controller
 public class ContractController {
-	//TODO Tyler
-	
+
+	//Display List For: Contract
+    @RequestMapping(value = "/contract_list", params = {"query"}, method = RequestMethod.GET)
+    public ModelAndView displayList(HttpServletRequest request, @RequestParam(value = "query") String query) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: Contract
+    	ContractDAO contractDAO = new ContractDAO();
+    	
+    	//Validate Session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to list for: Contract
+	    	model.addObject("redirectTo", "/contract_list?query=" + query);
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Get ArrayList From Query For: Contract
+	    	ArrayList<Contract> contractList = contractDAO.getListByQuery(query);
+		    //Update session user's personalized filter from the provided query for: Contract
+		    userSessionDAO.updateUsersFilter("Contract", query);
+	    	
+	    	//Call List View For: Contract
+	    	model = new ModelAndView("contract_list");
+	    	//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass ArrayList to View For: Contract
+	    	model.addObject("contractList", contractList);
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Add Form For: Contract
+    @RequestMapping(value = "/add_contract", method = RequestMethod.GET)
+    public ModelAndView displayNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: Contract
+    		model.addObject("redirectTo", "/add_contract");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: Contract
+    		model = new ModelAndView("contract_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+    		//Pass Operation of New
+    		model.addObject("operation", "new");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Add Form For: Contract
+    @RequestMapping(value = "/add_contract", method = RequestMethod.POST)
+    public ModelAndView insertNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: Contract
+    	ContractDAO contractDAO = new ContractDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: Contract
+    		model.addObject("redirectTo", "/add_contract");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not created.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: Contract
+    		
+    		//TODO Insert Base Object For: Contract
+    		
+    		//Redirect to list for: Contract
+    		//Include standard filter for: Contract
+    		model = new ModelAndView("redirect:/contract_list?query=" + userSessionDAO.getUsersFilter("Contract"));
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Update Form For: Contract
+    @RequestMapping(value = "/update_contract", params = {"id"}, method = RequestMethod.GET)
+    public ModelAndView displayExistingUser(HttpServletRequest request, @RequestParam(value = "id") Integer id) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: Contract
+    		model.addObject("redirectTo", "/update_contract?id=" +  id.toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: contract
+    		model = new ModelAndView("contract_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass Operation of Update
+    		model.addObject("operation", "update");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Update Form For: Contract
+    @RequestMapping(value = "/update_contract", method = RequestMethod.POST)
+    public ModelAndView updateExistingUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: Contract
+    	ContractDAO contractDAO = new ContractDAO();
+    	
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: Contract
+    		model.addObject("redirectTo", "/update_contract?id=" +  request.getParameter("id").toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not updated.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: contract
+    		
+    		//TODO Update Base Object For: contract
+    		
+    		//Redirect to list for: contract
+    		//Include standard filter for: contract
+    		model = new ModelAndView("redirect:/contract_list?query=" + userSessionDAO.getUsersFilter("Contract"));
+    	}
+    	
+    	return model;
+    }
 }
