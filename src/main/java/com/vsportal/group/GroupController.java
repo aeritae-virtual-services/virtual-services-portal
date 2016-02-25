85a1c78eea4872edd4de190af078aeb1c8ef9c4d
TylerSeanRau <trau@aeritae.com>
2016-02-26 01:15:39 +0900

    Filled in GroupController

--- src/main/java/com/vsportal/group/GroupController.java
+++ src/main/java/com/vsportal/group/GroupController.java
@@ -1,9 +1,190 @@
 package com.vsportal.group;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vsportal.group.Group;
+import com.vsportal.group.GroupDAO;
+import com.vsportal.user.UserDAO;
+import com.vsportal.utils.SessionHelper;
 
 @Controller
 public class GroupController {
-	//TODO Tyler
-	
+
+	//Display List For: Group
+    @RequestMapping(value = "/group_list", params = {"query"}, method = RequestMethod.GET)
+    public ModelAndView displayList(HttpServletRequest request, @RequestParam(value = "query") String query) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: Group
+    	GroupDAO groupDAO = new GroupDAO();
+    	
+    	//Validate Session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to list for: Group
+	    	model.addObject("redirectTo", "/group_list?query=" + query);
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Get ArrayList From Query For: Group
+	    	ArrayList<Group> groupList = groupDAO.getListByQuery(query);
+		    //Update session user's personalized filter from the provided query for: Group
+		    userSessionDAO.updateUsersFilter("Group", query);
+	    	
+	    	//Call List View For: Group
+	    	model = new ModelAndView("group_list");
+	    	//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass ArrayList to View For: Group
+	    	model.addObject("groupList", groupList);
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Add Form For: Group
+    @RequestMapping(value = "/add_group", method = RequestMethod.GET)
+    public ModelAndView displayNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: Group
+    		model.addObject("redirectTo", "/add_group");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: Group
+    		model = new ModelAndView("group_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+    		//Pass Operation of New
+    		model.addObject("operation", "new");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Add Form For: Group
+    @RequestMapping(value = "/add_group", method = RequestMethod.POST)
+    public ModelAndView insertNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: Group
+    	GroupDAO groupDAO = new GroupDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: group
+    		model.addObject("redirectTo", "/add_group");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not created.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: Group
+    		
+    		//TODO Insert Base Object For: Group
+    		
+    		//Redirect to list for: Group
+    		//Include standard filter for: Group
+    		model = new ModelAndView("redirect:/group_list?query=" + userSessionDAO.getUsersFilter("Group"));
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Update Form For: Group
+    @RequestMapping(value = "/update_group", params = {"id"}, method = RequestMethod.GET)
+    public ModelAndView displayExistingUser(HttpServletRequest request, @RequestParam(value = "id") Integer id) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: Group
+    		model.addObject("redirectTo", "/update_group?id=" +  id.toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: Group
+    		model = new ModelAndView("group_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass Operation of Update
+    		model.addObject("operation", "update");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Update Form For: Group
+    @RequestMapping(value = "/update_group", method = RequestMethod.POST)
+    public ModelAndView updateExistingUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: Group
+    	GroupDAO groupDAO = new GroupDAO();
+    	
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: Group
+    		model.addObject("redirectTo", "/update_group?id=" +  request.getParameter("id").toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not updated.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: group
+    		
+    		//TODO Update Base Object For: group
+    		
+    		//Redirect to list for: group
+    		//Include standard filter for: group
+    		model = new ModelAndView("redirect:/group_list?query=" + userSessionDAO.getUsersFilter("Group"));
+    	}
+    	
+    	return model;
+    }
 }
