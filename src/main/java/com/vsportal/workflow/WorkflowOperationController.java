e69b5044b7c7c7be30683826e6becea276053241
TylerSeanRau <trau@aeritae.com>
2016-02-25 12:38:42 +0900

    TierController, WorkflowOperationController, WorkflowStepController

--- src/main/java/com/vsportal/workflow/WorkflowOperationController.java
+++ src/main/java/com/vsportal/workflow/WorkflowOperationController.java
@@ -1,9 +1,189 @@
 package com.vsportal.workflow;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vsportal.workflow.WorkflowOperation;
+import com.vsportal.workflow.WorkflowOperationDAO;
+import com.vsportal.user.UserDAO;
+import com.vsportal.utils.SessionHelper;
 
 @Controller
 public class WorkflowOperationController {
-	//TODO Tyler
-	
+
+	//Display List For: WorkflowOperation
+    @RequestMapping(value = "/workflowOperation_list", params = {"query"}, method = RequestMethod.GET)
+    public ModelAndView displayList(HttpServletRequest request, @RequestParam(value = "query") String query) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: WorkflowOperation
+    	WorkflowOperationDAO workflowOperationDAO = new WorkflowOperationDAO();
+    	
+    	//Validate Session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to list for: WorkflowOperaration
+	    	model.addObject("redirectTo", "/workflowOperation_list?query=" + query);
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Get ArrayList From Query For: workflowOperation
+	    	ArrayList<WorkflowOperation> workflowOperationList = WorkflowOperationDAO.getListByQuery(query);
+		    //Update session user's personalized filter from the provided query for: WorkflowOperation
+		    userSessionDAO.updateUsersFilter("WorkflowOperation", query);
+	    	
+	    	//Call List View For: WorkflowOperation
+	    	model = new ModelAndView("workflowOperation_list");
+	    	//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass ArrayList to View For: workflowOperation
+	    	model.addObject("workflowOperationList", workflowOperationList);
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Add Form For: WorkflowOperation
+    @RequestMapping(value = "/add_workflowOperation", method = RequestMethod.GET)
+    public ModelAndView displayNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: WorkflowOperation
+    		model.addObject("redirectTo", "/add_workflowOperation");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: WorkflowOperation
+    		model = new ModelAndView("workflowOperation_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+    		//Pass Operation of New
+    		model.addObject("operation", "new");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Add Form For: WorkflowOperation
+    @RequestMapping(value = "/add_workflowOperation", method = RequestMethod.POST)
+    public ModelAndView insertNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: WorkflowOperation
+    	WorkflowOperationDAO workflowOperationDAO = new WorkflowOperationDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: WorkflowOperation
+    		model.addObject("redirectTo", "/add_workflowOperation");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not created.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: workflowOperation
+    		
+    		//TODO Insert Base Object For: workflowOperation
+    		
+    		//Redirect to list for: workflowOperation
+    		//Include standard filter for: workflowOperation
+    		model = new ModelAndView("redirect:/workflowOperation_list?query=" + userSessionDAO.getUsersFilter("WorkflowOperation"));
+    	}
+    	return model;
+    }
+    
+    //Display Update Form For: workflowOperation
+    @RequestMapping(value = "/update_workflowOperation", params = {"id"}, method = RequestMethod.GET)
+    public ModelAndView displayExistingUser(HttpServletRequest request, @RequestParam(value = "id") Integer id) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: workflowOperation
+    		model.addObject("redirectTo", "/update_workflowOperation?id=" +  id.toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: WorkflowOperation
+    		model = new ModelAndView("workflowOperation_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass Operation of Update
+    		model.addObject("operation", "update");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Update Form For: workflowOperation
+    @RequestMapping(value = "/update_workflowOperation", method = RequestMethod.POST)
+    public ModelAndView updateExistingUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: workflowOperation
+    	WorkflowOperationDAO workflowOperationDAO = new WorkflowOperationDAO();
+    	
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: workFlowOperation
+    		model.addObject("redirectTo", "/update_workflowOperation?id=" +  request.getParameter("id").toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not updated.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: workflowOperation
+    		
+    		//TODO Update Base Object For: workflowOperation
+    		
+    		//Redirect to list for: workflowOperation
+    		//Include standard filter for: workflowOperation
+    		model = new ModelAndView("redirect:/workflowOperation_list?query=" + userSessionDAO.getUsersFilter("WorkflowOperation"));
+    	}
+    	
+    	return model;
+    }
 }
--- src/main/java/com/vsportal/workflow/WorkflowStepController.java
+++ src/main/java/com/vsportal/workflow/WorkflowStepController.java
@@ -1,8 +1,190 @@
 package com.vsportal.workflow;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vsportal.workflow.WorkflowStep;
+import com.vsportal.workflow.WorkflowStepDAO;
+import com.vsportal.user.UserDAO;
+import com.vsportal.utils.SessionHelper;
 
 @Controller
 public class WorkflowStepController {
-	//TODO Tyler
+
+	//Display List For: WorkflowStep
+    @RequestMapping(value = "/workflowStep_list", params = {"query"}, method = RequestMethod.GET)
+    public ModelAndView displayList(HttpServletRequest request, @RequestParam(value = "query") String query) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: workflowStep
+    	WorkflowStepDAO workflowStepDAO = new WorkflowStepDAO();
+    	
+    	//Validate Session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to list for: WorkflowStep
+	    	model.addObject("redirectTo", "/workflowStep_list?query=" + query);
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Get ArrayList From Query For: WorkflowStep
+	    	ArrayList<WorkflowStep> workflowStepList = workflowStepDAO.getListByQuery(query);
+		    //Update session user's personalized filter from the provided query for: WorkflowStep
+		    userSessionDAO.updateUsersFilter("WorkflowStep", query);
+	    	
+	    	//Call List View For: WorkflowStep
+	    	model = new ModelAndView("workflowStep_list");
+	    	//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass ArrayList to View For: WorkflowStep
+	    	model.addObject("workflowStepList", workflowStepList);
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Add Form For: WorkflowStep
+    @RequestMapping(value = "/add_workflowStep", method = RequestMethod.GET)
+    public ModelAndView displayNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: WorkflowStep
+    		model.addObject("redirectTo", "/add_workflowStep");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: WorkflowStep
+    		model = new ModelAndView("workflowStep_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+    		//Pass Operation of New
+    		model.addObject("operation", "new");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Add Form For: WorkflowStep
+    @RequestMapping(value = "/add_workflowStep", method = RequestMethod.POST)
+    public ModelAndView insertNewUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: WorkflowStep
+    	WorkflowStepDAO workflowStepDAO = new WorkflowStepDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to new form for: WorkflowStep
+    		model.addObject("redirectTo", "/add_workflowStep");
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not created.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: WorkflowStep
+    		
+    		//TODO Insert Base Object For: WorkflowStep
+    		
+    		//Redirect to list for: WorkflowStep
+    		//Include standard filter for: WorkflowStep
+    		model = new ModelAndView("redirect:/worflowStep_list?query=" + userSessionDAO.getUsersFilter("WorkflowStep"));
+    	}
+    	
+    	return model;
+    }
+    
+    //Display Update Form For: WorkflowStep
+    @RequestMapping(value = "/update_workflowStep", params = {"id"}, method = RequestMethod.GET)
+    public ModelAndView displayExistingUser(HttpServletRequest request, @RequestParam(value = "id") Integer id) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Validate session
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: WorkflowStep
+    		model.addObject("redirectTo", "/update_workflowStep?id=" +  id.toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: Please log in.");
+    	} else {
+    		//Call Form View For: WorkflowStep
+    		model = new ModelAndView("workflowStep_form");
+    		//Pass session user to View
+	    	model.addObject("sessionUser", userSessionDAO.getSessionUser(sess));
+	    	//Pass Operation of Update
+    		model.addObject("operation", "update");
+    	}
+    	
+    	return model;
+    }
+    
+    //Submit Update Form For: WorkflowStep
+    @RequestMapping(value = "/update_workflowStep", method = RequestMethod.POST)
+    public ModelAndView updateExistingUser(HttpServletRequest request) {
+    	HttpSession sess = request.getSession();
+    	SessionHelper sh = new SessionHelper();
+    	ModelAndView model = null;
+    	
+    	//Get User Data Access Object to Manage Session User
+    	UserDAO userSessionDAO = new UserDAO();
+    	
+    	//Get Data Access Object For: WorkflowStep
+    	WorkflowStepDAO workflowStepDAO = new WorkflowStepDAO();
+    	
+    	if(!sh.isValidSession(sess)) {
+    		//If invalid session, redirect to login page
+    		model = new ModelAndView("login");
+    		//Set redirect back to existing form for: WorkflowStep
+    		model.addObject("redirectTo", "/update_workflowStep?id=" +  request.getParameter("id").toString());
+    		//Error Message: Invalid session
+    		model.addObject("errmsg", "Invalid Session: User record not updated.");
+    	} else {
+    		//TODO Get All Attributes From Form
+    		
+    		//TODO Populate Object with Attributes For: WorkflowStep
+    		
+    		//TODO Update Base Object For: WorkflowStep
+    		
+    		//Redirect to list for: WorkflowStep
+    		//Include standard filter for: WorkflowStep
+    		model = new ModelAndView("redirect:/workflowStep_list?query=" + userSessionDAO.getUsersFilter("WorkflowStep"));
+    	}
+    	
+    	return model;
+    }
 }
