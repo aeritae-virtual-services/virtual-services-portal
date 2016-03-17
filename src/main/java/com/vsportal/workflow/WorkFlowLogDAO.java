8deb29029c68bbb514a107f553e51f56dbca5c0d
zorgdrager <jzorgdrager@aeritae.com>
2016-03-17 12:56:51 -0500

    userDAO

---
+++ src/main/java/com/vsportal/workflow/WorkFlowLogDAO.java
@@ -0,0 +1,12 @@
+package com.vsportal.workflow;
+
+import com.vsportal.request.Request;
+
+public class WorkFlowLogDAO {
+
+	public static WorkflowLog getCurrentLog(Request req) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
